1556d4c4a752007b34b4f560f369f1f728c2af07
Sardor Taylakov <taylak_s1@denison.edu>
2019-03-15 03:58:19 -0400

    added profiles

--- shop/models.py
+++ shop/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-# from django.urls import reverse
+from django.urls import reverse
 
 
 class Category(models.Model):
@@ -16,9 +16,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #         return reverse('shop:product_list_by_category',
-    #                        args=[self.slug])
+    def get_absolute_url(self):
+            return reverse('shop:product_list_by_category',
+                           args=[self.slug])
 
 
 class Product(models.Model):
@@ -42,6 +42,6 @@ class Product(models.Model):
     def __str__(self):
         return self.name
 
-    # def get_absolute_url(self):
-    #         return reverse('shop:product_detail',
-    #                        args=[self.id, self.slug])
+    def get_absolute_url(self):
+            return reverse('shop:product_detail',
+                           args=[self.id, self.slug])
